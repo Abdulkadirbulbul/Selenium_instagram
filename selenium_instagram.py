e4a78391d2ec7f2f4e150e4350b60ee08400ee9c
abdulkadirbulbul <kadirwinchester25@gmail.com>
2022-07-19 11:14:29 +0300

    json doyası eklendi

--- selenium_instagram.py
+++ selenium_instagram.py
@@ -1,27 +1,42 @@
-from selenium import webdriver  #kütüphanelerin import edilmesi
+#kütüphanelerin import edilmesi
+from selenium import webdriver  
 import time                     
-browser=webdriver.Firefox()     #browser olarak firefox tanımlanması
+import json
+
+#kullanıcıadı ve şifremi json dosyasından alıyorum.
+jsonFile = open('./secret.json') 
+jsonData = json.load(jsonFile)   
+
+
+#browser olarak firefox tanımlanması
+browser=webdriver.Firefox()    
+
 
 time.sleep(5) 
 
-browser.get("https://www.instagram.com/")        #Urlye gitmesi
+#Urlye gitmesi
+browser.get("https://www.instagram.com/")        
  
-time.sleep(10)
+time.sleep(5)
 
+ #username elementini sayfada arat ve usernameye ata
+username = browser.find_element("name", "username")   
 
-username = browser.find_element("name", "username")          #username elementini sayfada arat ve usernameye ata
-password = browser.find_element("name", "password")          #password elementini sayfada arat ve password ata
+ #password elementini sayfada arat ve password ata    
+password = browser.find_element("name", "password")         
 
-
-username.send_keys("kadirbulbulcom")                         #kullanıcı adınız
-password.send_keys("parolamyok")                             #parolanız
+ #kullanıcı adınız
+username.send_keys(jsonData['username']) 
+ #parolanız                       
+password.send_keys(jsonData['password'])                            
 
 
 #Burada ise sitede DOM kullanarak , xpath yolu verilen  elementi giris_yap değişkenine atıyoruz.
 
 giris_yap=browser.find_element("xpath","/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]") 
-giris_yap.click()  # Giriş yap değişkenine tıklansın
+# Giriş yap değişkenine tıklansın
 
+giris_yap.click()  
 time.sleep(10)
 
 browser.close()
